51fc53163fdb69af65248f21a9d9ac0d1c02e6d3
Aldin <alhuric89@gmail.com>
2021-10-27 21:20:41 +0200

    add boolean advanced.js

---
+++ basic/boolean.js
@@ -0,0 +1,27 @@
+let temp = 180;
+let isFreezing = temp !== 32;
+
+if(isFreezing){
+    console.log(`It is fing cold`)
+}
+
+if(temp >= 110) {
+    console.log("It is fking hot");
+}
+
+
+
+let age = 66;
+/*
+let isChild = age <=7;
+let isSenior = age >= 65;
+console.log(isChild, isSenior);
+*/
+
+if(age <= 7) {
+    console.log("You got child discount");
+}
+
+if(age >=65) {
+    console.log("You got senior discount");
+}
