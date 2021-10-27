0ede32b9c93910fc1c919991f661e955236ceec6
Aldin <alhuric89@gmail.com>
2021-10-27 15:58:06 +0200

    add numbers and strings js

---
+++ basic/strings.js
@@ -0,0 +1,12 @@
+let firstName = "Aldo";
+let lastName = "Hur"
+console.log(firstName +" " + lastName);
+
+
+
+let city = "Prijepolje";
+let country = "Serbia";
+
+location = city + " " + country;
+
+console.log(location);
